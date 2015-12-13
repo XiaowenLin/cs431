86076842310c697be4a2527a79a661fa72677253
Xiaowen Lin <xlin@illinois.edu>
2015-12-12 20:46:10 -0600

    disable recv

--- scripts/client.py
+++ scripts/client.py
@@ -15,30 +15,37 @@ from __init__ import *
 import json
 import socket               
 
-def send_msg(msg_s):
-   """
-   >>> msg = {'topic': 'coordinates',
-              'origin': (0, 0),
-              'destination': (1, 1)}
-   >>> msg_s = json.dumps(msg)
-   >>> send_msg(msg_s)
-   200
-   """
-   # create a socket object
-   s = socket.socket()         
-   s.connect((TCP_HOST, TCP_PORT))
+class Commander:
+    def __init__(self):
+        # create a socket object
+        self.s = socket.socket()
+        self.s.connect((TCP_HOST, TCP_PORT))
 
+    def send_msg(msg_s):
+       """
+       >>> msg = {'topic': 'coordinates',
+                  'origin': (0, 0),
+                  'destination': (1, 1)}
+       >>> msg_s = json.dumps(msg)
+       >>> send_msg(msg_s)
+       200
+       """
+       s.send(msg_s)
+       # recv_s = s.recv(BUFFER_SIZE)
+       # recv = json.loads(recv_s)
+       # logging.debug(recv['status'])
+       # return recv
+       logging.info('finish sending')
+       return
 
-   s.send(msg_s)
-   recv_s = s.recv(BUFFER_SIZE)
-   recv = json.loads(recv_s)
-   logging.debug(recv['status'])
-   s.close                     # Close the socket when done
-   return recv
+    def close(self):
+        self.s.close                     # Close the socket when done
 
 if __name__ == '__main__':
     msg = {'topic': 'coordinates',
            'origin': (0, 0),
            'destination': (1, 1)}
     msg_s = json.dumps(msg)
-    send_msg(msg_s)
+    cmd = Commander()
+    cmd.send_msg(msg_s)
+    cmd.close()
