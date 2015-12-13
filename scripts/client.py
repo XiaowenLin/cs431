96a9e682b8fe4795af4e4c1d337115d3bc77f851
Xiaowen Lin <xlin@illinois.edu>
2015-12-12 21:30:48 -0600

    update client with threading

--- scripts/client.py
+++ scripts/client.py
@@ -13,13 +13,18 @@ features:
 """
 from __init__ import *
 import json
-import socket               
+import socket
+import threading
 
 class Commander:
     def __init__(self):
         # create a socket object
         self.s = socket.socket()
         self.s.connect((TCP_HOST, TCP_PORT))
+
+    def send_worker(self, msg_s):
+        t = threading.Thread(target=self.send_msg, args=(msg_s, ))
+        t.start()
 
     def send_msg(self, msg_s):
        """
@@ -30,16 +35,25 @@ class Commander:
        >>> send_msg(msg_s)
        200
        """
-       s.send(msg_s)
-       # recv_s = s.recv(BUFFER_SIZE)
-       # recv = json.loads(recv_s)
-       # logging.debug(recv['status'])
-       # return recv
-       logging.info('finish sending')
-       return
+       self.s.send(msg_s)
+       logging.info('waiting for server response')
+       recv_s = s.recv(BUFFER_SIZE)
+       recv = json.loads(recv_s)
+       if recv:
+           logging.debug(recv['status'])
+           logging.info('finish sending')
+           return recv
+       else:
+           return
 
     def close(self):
-        self.s.close                     # Close the socket when done
+        for t in threading.enumerate():
+            if t is main_thread:
+                continue
+                logging.debug('joining %s', t.getName())
+                t.join()
+            self.s.close                     # Close the socket when done
+        logging.debug('close success')
 
 if __name__ == '__main__':
     msg = {'topic': 'coordinates',
@@ -47,5 +61,5 @@ if __name__ == '__main__':
            'destination': (1, 1)}
     msg_s = json.dumps(msg)
     cmd = Commander()
-    cmd.send_msg(msg_s)
+    cmd.send_worker(msg_s)
     cmd.close()
