454a46b275fa71252e13c1ba7efab52f0a1cb0d2
Ronald Wright <logiconcepts819@gmail.com>
2015-12-12 19:39:43 -0600

    obstacle-avoider: Add missing initialization of TTC server

--- obstacle-avoidance/main.py
+++ obstacle-avoidance/main.py
@@ -22,17 +22,24 @@ def balance_strategy(left_ttc, right_ttc):
 
 def main(argv):
     global frame_server, ttc_server
+
     frame_server = FrameServer()
     ttc_server = TTCServer()
+
     frame_server.run_daemon_thread()
+    ttc_server.run_daemon_thread()
+
     avoider = ObstacleAvoider()
     avoider.set_imgdisp_cb(imgdisp)
     avoider.set_balance_strategy_cb(balance_strategy)
     avoider.set_min_ttc_cb(min_ttc)
+
     print("\n")
     avoider.start()
+
     while threading.active_count() > 1:
         time.sleep(0.1)
+
     return 0
 
 if __name__ == "__main__":
